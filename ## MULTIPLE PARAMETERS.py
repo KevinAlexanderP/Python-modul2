fec730da0689d7dbc7630bca52567c680b1a6f7e
KevinAlexanderP <kennedyisdead13@gmail.com>
2020-02-24 21:49:30 -0600

    parameters

---
+++ ## MULTIPLE PARAMETERS.py
@@ -0,0 +1,15 @@
+## MULTIPLE PARAMETERS
+def greet_customer(grocery_store, special_item):
+    ## we have grocery store and special item as variables
+    print("Welcome to "+ grocery_store + ".")
+    print("Our special is " + special_item + ".")
+    print("Have fun shopping!")
+
+    ## functions, separated by commas
+
+print   greet_customer("Stus staples", "papaya")
+
+def mult_x_add_y(number,x,y):
+  print(number*x + y)
+
+mult_x_add_y(5,2,3)
